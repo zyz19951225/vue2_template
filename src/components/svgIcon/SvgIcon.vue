a0408058435f6da297f517df515c1c3cc5be1246
suju <274813601@qq.com>
2022-08-18 10:28:16 +0800

    pref:集成element-ui

--- src/components/svgIcon/SvgIcon.vue
+++ src/components/svgIcon/SvgIcon.vue
@@ -51,8 +51,8 @@ export default {
 
 <style scoped>
 .svg-icon {
-  /* width: 1em;
-  height: 1em; */
+  width: 1em;
+  height: 1em;
   vertical-align: -0.15em;
   fill: currentColor;
   overflow: hidden;
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 //引入store
 import store from './store'
 import "./icons"
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
 
